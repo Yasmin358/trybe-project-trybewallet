624b979577140834e1db8d5de532ef4e4f46ddb5
Yasmin358 <amorin.yasmin378@gmail.com>
2022-07-28 21:41:50 -0300

    Resolução requuisito 1 - primeira tentativa

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,79 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { saveEmail } from '../redux/actions';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+    buttonDisabled: true,
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    }, this.validateButton);
+  }
+
+  validateButton = () => {
+    const { email, password } = this.state;
+    const minLength = 3;
+    if (password.length >= minLength && email.includes('@') && email.includes('.com')) {
+      this.setState({ buttonDisabled: false });
+    } else {
+      this.setState({ buttonDisabled: true });
+    }
+  }
+
+  submitLogin = () => {
+    const { email } = this.state;
+    const { history, setEmail } = this.props;
+    setEmail(email);
+    history.push('/carteira');
+  }
+
   render() {
-    return <div>Page Login</div>;
+    const { email, password, buttonDisabled } = this.state;
+    return (
+      <form>
+        <input
+          type="email"
+          data-testid="email-input"
+          placeholder="Digite seu email aqui"
+          name="email"
+          value={ email }
+          onChange={ this.handleChange }
+        />
+        <input
+          type="password"
+          data-testid="password-input"
+          onChange={ this.handleChange }
+          placeholder="Digite sua senha aqui"
+          name="password"
+          value={ password }
+        />
+        <button
+          type="button"
+          disabled={ buttonDisabled }
+          onClick={ this.submitLogin }
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  history: PropTypes.objectOf(PropTypes.any).isRequired,
+  setEmail: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  setEmail: (email) => dispatch(saveEmail(email)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,5 @@
 // Coloque aqui suas actions
+
+export const USER_EMAIL = 'USER_EMAIL';
+
+export const saveEmail = (email) => ({ type: USER_EMAIL, email });
