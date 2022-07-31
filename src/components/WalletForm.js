5326425e4cd0e608387a3eda67f8b5024313b6ae
Yasmin358 <amorin.yasmin378@gmail.com>
2022-07-31 15:36:40 -0300

    Resolução requisito 3

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -1,9 +1,67 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { fetchApi } from '../redux/actions';
 
-export default class WalletForm extends Component {
+class WalletForm extends React.Component {
+  componentDidMount() {
+    const { getCurrencies } = this.props;
+    getCurrencies();
+  }
+
   render() {
+    const { currencies } = this.props;
     return (
-      <div>WalletForm</div>
+      <form>
+        <label htmlFor="valor">
+          Valor:
+          <input type="number" data-testid="value-input" id="valor" />
+        </label>
+
+        <label htmlFor="moeda">
+          Moeda:
+          <select id="moeda" data-testid="currency-input">
+            {currencies.map((moeda) => <option key={ moeda }>{moeda}</option>)}
+          </select>
+        </label>
+
+        <label htmlFor="pagamento">
+          <select id="pagamento" data-testid="method-input">
+            <option>Dinheiro</option>
+            <option>Cartão de crédito</option>
+            <option>Cartão de débito</option>
+          </select>
+        </label>
+
+        <label htmlFor="categoria">
+          <select id="categoria" data-testid="tag-input">
+            <option>Alimentação</option>
+            <option>Lazer</option>
+            <option>Trabalho</option>
+            <option>Transporte</option>
+            <option>Saúde</option>
+          </select>
+        </label>
+
+        <label htmlFor="descrição">
+          <input type="text" data-testid="description-input" id="descrição" />
+        </label>
+      </form>
     );
   }
 }
+
+WalletForm.propTypes = {
+  currencies: PropTypes.arrayOf(PropTypes.string).isRequired,
+  getCurrencies: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  getCurrencies: () => dispatch(fetchApi()),
+});
+
+const mapStateToProps = (state) => ({
+  currencies: state.wallet.currencies,
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(WalletForm);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,5 +1,19 @@
 // Coloque aqui suas actions
 
 export const USER_EMAIL = 'USER_EMAIL';
+export const GET_CURRENCIES = 'GET_CURRENCIES';
+export const REQUEST_API = 'REQUEST_API';
 
 export const saveEmail = (email) => ({ type: USER_EMAIL, email });
+export const requestApi = () => ({ type: REQUEST_API });
+export const getCurrencies = (currencies) => ({ type: GET_CURRENCIES, currencies });
+
+export function fetchApi() {
+  return (dispatch) => {
+    dispatch(requestApi());
+    return fetch('https://economia.awesomeapi.com.br/json/all')
+      .then((response) => response.json())
+      .then((currencies) => dispatch(getCurrencies(Object.keys(currencies)
+        .filter((moeda) => moeda !== 'USDT'))));
+  };
+}
